c0b834b8349cfb6638fc64a346c43684724865dd
Евгений Гужихин <gpont97@gmail.com>
2021-09-27 21:44:14 +0300

    some fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import {SecretManagerServiceClient} from "@google-cloud/secret-manager";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import * as express from "express";
 import * as cors from "cors";
 import {FeedbackRecord} from "./Models/FeedbackRecord";
@@ -15,38 +15,64 @@ const secretManagerClient = new SecretManagerServiceClient();
 admin.initializeApp();
 app.use(cors());
 app.use(captchaValidator);
+app.use(handleError);
 
 async function captchaValidator(request: express.Request, response: express.Response, next: express.NextFunction) {
-  const secret = await getCaptchaKey();
-  const token = request.header("ReCaptchaToken") || "";
-  const params = new URLSearchParams();
-  params.append("secret", secret);
-  params.append("response", token);
-  const siteVerifyResponse = axios.post("https://www.google.com/recaptcha/api/siteverify", params);
-  const data = await (await siteVerifyResponse).data as SitreverifyResponse;
-  functions.logger.log("Verify response data", data);
-  if (data.success) {
-    next();
-  } else {
-    response.sendStatus(403).end();
+  try {
+    const { data: siteverifyResponse } = await axios.post<object, AxiosResponse<SitreverifyResponse>>(
+      "https://www.google.com/recaptcha/api/siteverify",
+      new URLSearchParams({
+        secret: await getCaptchaKey(),
+        response: request.header("ReCaptchaToken") || ""
+      })
+    );
+
+    if (siteverifyResponse.success) {
+      next();
+    } else {
+      response.sendStatus(403).end();
+    }
+  } catch (err) {
+    next(err);
   }
 }
 
-async function getCaptchaKey(): Promise<string> {
-  const [secret] = await secretManagerClient.accessSecretVersion({
-    name: captchaPrivateKeyName,
-  });
-  const key = secret.payload?.data?.toString();
-  return key?.toString() || "";
+function handleError(err: Error, request: express.Request, response: express.Response) {
+  response.status(500).send(err.message).end();
 }
 
+async function getCaptchaKey(): Promise<string> {
+  if (process.env.DEBUG) {
+    return process.env.SECRET_KEY;
+  }
+
+  const [ secret ] = await secretManagerClient.accessSecretVersion({
+    name: captchaPrivateKeyName,
+  });
+
+  if (!secret.payload?.data) {
+    throw Error("Captcha private key was not set");
+  }
+
+  return secret.payload.data as string;
+}
+
+const isCorrectFeedback = <T>(value: FeedbackRecord | T): value is FeedbackRecord => value !== null &&
+  typeof value === "object" &&
+  ["username", "email", "message"].every((key: string) => Object.keys(value).includes(key));
+
 app.post("/", async (request, response) => {
-  const feedbackRecord = request.body as FeedbackRecord;
-  functions.logger.log("Feedback record", feedbackRecord);
+  const { body } = request;
+
+  if (!isCorrectFeedback(body)) {
+    throw Error(`Incorrect request body: ${body}`);
+  }
+
   await admin
-      .firestore()
-      .collection("feedbackRecords")
-      .add(feedbackRecord);
+    .firestore()
+    .collection("feedbackRecords")
+    .add(body);
+  
   response.sendStatus(200).end();
 });
 
